d4ac37eef718b80ce940a1e1f16c256c22528a10
iochenlei <iochenlei@gmail.com>
2019-12-15 14:42:51 +0800

    rename package name

--- user-management/src/main/java/com/lovecode/user/UserManagementApplication.java
+++ user-management/src/main/java/com/lovecode/user/UserManagementApplication.java
@@ -1,4 +1,4 @@
-package com.lovecode.usermanagement;
+package com.lovecode.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
